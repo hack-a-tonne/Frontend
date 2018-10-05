c2dd9301f9274e39eaf9bc2cf75b3aa66a0c804d
Andy1993 <andy-gloeckner@gmx.de>
2018-10-05 23:26:26 +0200

    Images und Map-Position geändert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
-
-
 var map = new Map('map');
+var sensor = new Sensor(1,'test',51.9544809, 7.6104615);
+map.draw(sensor, function () {
+        $.ajax({
+            url: "http://tonne.kevit.info/getData"
+        }).then(function(data) {
+           $('.greeting-id').append(data.id);
+           $('.greeting-content').append(data.content);
+        });
+        alert("message");
+    });
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,24 @@
 class Map {
-
     constructor(selector) {
+        this.sensors = [];
         this.map = L.map(selector).setView([51.9475746, 7.6007236], 15);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }
         ).addTo(this.map);
+    }
+    draw(sensor, onclick) {
+        var marker = L.marker([sensor.lat,sensor.lng]).addTo(this.map);
+        marker.on('click', onclick);
+    }
+}
+
+class Sensor {
+    constructor(id,deviceName,lat,lng) {
+        this.id = id;
+        this.deviceName = deviceName;
+        this.lat = lat;
+        this.lng = lng;
     }
 }
