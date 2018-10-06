5077f331dcf7c069454e5c0f5553a009962c6020
Andy1993 <andy-gloeckner@gmx.de>
2018-10-06 10:46:31 +0200

    GetAlLDevices Sensore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 var map = new Map('map');
-var sensor = new Sensor(1,'test',51.9544809, 7.6104615);
-map.draw(sensor, function () {
-        $.ajax({
-            url: "http://tonne.kevit.info/getData"
-        }).then(function(data) {
-           $('.greeting-id').append(data.id);
-           $('.greeting-content').append(data.content);
+
+var latlng = map.map.getCenter();
+var zoom = map.map.getZoom();
+
+$(document).ready(function () {
+    $.ajax({
+        url: "https://hackertonne.azurewebsites.net/api/GetAllDevices?lat=" + latlng.lat + "&lng=" + latlng.lng + "&zoom=" + zoom,
+        type: 'GET',
+        dataType: 'json'
+    }).then(function (data) {
+        data.forEach(element => {
+            map.draw(new Sensor(element.id, element.deviceName, element.lat, element.lng),
+                function () {
+                    alert(element.id);
+                }
+            );
         });
-        alert("message");
     });
+});
