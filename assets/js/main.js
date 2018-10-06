53ae8d7727292e570e16b9610ccecdafff9a912f
m-wiesmann <magnus.wiesmann@aschendorff.de>
2018-10-06 14:21:21 +0200

    add click on marker function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Sitebar Logic
-var device_id = 14;
-var sidebar = new Sidebar('.value-sidebar', 'Sonde ' + device_id);
-$(document).ready(
+
+function showSondenSitebar(device_id) {
+    var sidebar = new Sidebar('.value-sidebar', 'Sonde ' + device_id);
     $.ajax({
         url: "https://hackertonne.azurewebsites.net/api/GetValuesByDevice?device_id=" + device_id,
         type: 'GET',
@@ -14,7 +14,12 @@ $(document).ready(
             var htmlElement = sidebar.setElement(data.datasets[i].label, current_value, data.datasets[i].data);
         }
     })
-);
+}
+var device_id = 14;
+
+$(document).ready(function () {
+    showSondenSitebar(device_id);
+});
 
 // Map Logic
 var map = new Map('map');
